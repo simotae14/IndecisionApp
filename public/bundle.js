1c59e95c5026bf07003341379fae773bf0dbf6ee
Simotae14 <startae14@gmail.com>
2017-11-28 23:02:18 +0100

    ES6 import / export

--- public/bundle.js
+++ public/bundle.js
@@ -65,9 +65,40 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-console.log('app.js is running');
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__person_js__ = __webpack_require__(2);
+//import './utils.js';
+// import { square, add } from './utils.js';
+
+
+// console.log('app.js is running');
+
+// console.log(square(4));
+
+// console.log(add(100, 23));
+
+
+
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["b" /* isAdult */](17));
+
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](21));
+
+
+/***/ }),
+/* 1 */,
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return isAdult; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return canDrink; });
+const isAdult = (age) => age >= 18;
+
+const canDrink = (age) => age >= 21;
+
 
 
 /***/ })
---
+++ src/utils.js
@@ -0,0 +1,7 @@
+console.log('utils.js running');
+
+export const square = (x) => x * x;
+
+export const add = (a, b) => a + b;
+
+//export { square, add };
