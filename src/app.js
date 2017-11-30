e35e1ed52a4750ac7cac468f9bfd94914c759f64
Simone Taeggi <simone.taeggi@leovegas.com>
2017-11-30 23:53:02 +0100

    Classi ES6

--- src/app.js
+++ src/app.js
@@ -5,3 +5,20 @@ import IndecisionApp from './components/IndecisionApp'
 // renderizzare il Component
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
 
+// vecchia sintassi Classi
+class VecchiaSintassi {
+    constructor() {
+        this.nome = "Mike";
+    }
+}
+
+const vecchiaSintassi = new VecchiaSintassi();
+console.log(vecchiaSintassi);
+
+// nuova sintassi Classi
+class NuovaSintassi {
+    nome = "Jen";
+}
+
+const nuovaSintassi = new NuovaSintassi();
+console.log(nuovaSintassi);
