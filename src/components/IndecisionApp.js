71739484db583add71deb7444d12f73832b95027
Simone Taeggi <simone.taeggi@leovegas.com>
2017-12-08 19:06:30 +0100

    dare stile all'elenco delle options

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -80,14 +80,16 @@ export default class IndecisionApp extends React.Component {
               handlePick={this.handlePick}
               hasOptions={this.state.options.length > 0}
             />
-            <Options
-              options={this.state.options}
-              handleDeleteOptions={this.handleDeleteOptions}
-              handleDeleteOption={this.handleDeleteOption}
-            />
-            <AddOption
-              handleAddOption={this.handleAddOption}
-            />
+            <div className="widget">
+              <Options
+                options={this.state.options}
+                handleDeleteOptions={this.handleDeleteOptions}
+                handleDeleteOption={this.handleDeleteOption}
+              />
+              <AddOption
+                handleAddOption={this.handleAddOption}
+              />
+            </div>
           </div>
 
           <OptionModal 
--- src/components/Options.js
+++ src/components/Options.js
@@ -5,11 +5,15 @@ import Option from "./Option";
 // stateless
 const Options = (props) => (
   <div>
-    <button 
-      className="button button--link"
-      onClick={props.handleDeleteOptions
-    }>Rimuovi tutto</button>
-    {props.options.length === 0 && <p>Per favore inserisci una opzione per iniziare!</p>}
+    <div className="widget-header">
+      <h3 className="widget-header__title">Le Tue Opzioni</h3>
+      <button 
+        className="button button--link"
+        onClick={props.handleDeleteOptions
+      }>Rimuovi tutto</button>
+    </div>
+
+    {props.options.length === 0 && <p className="widget__message">Per favore inserisci una opzione per iniziare!</p>}
     {
       props.options.map((option) => (
         <Option
