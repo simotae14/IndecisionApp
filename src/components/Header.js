ce481444012d89c8f27f06f7192c480dc047d687
Simone Taeggi <simone.taeggi@leovegas.com>
2017-12-08 17:34:50 +0100

    Tema e uso di variabili nel css

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,11 @@ import React from 'react';
 // creo il Component Header
 // stateless
 const Header = (props) => (
-  <div
-    className="header"
-  >
-    <h1
-      className="header__title"
-    >{props.title}</h1>
-    { props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2> }
+  <div className="header">
+    <div className="container">
+      <h1 className="header__title">{props.title}</h1>
+      { props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2> }
+    </div>
   </div>
 );
 
