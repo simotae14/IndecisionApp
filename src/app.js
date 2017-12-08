059330fc3c34b1280bff14815ba6160f03d82809
Simone Taeggi <simone.taeggi@leovegas.com>
2017-12-08 16:46:32 +0100

    Normalize.css

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp'
 
+import 'normalize.css/normalize.css';
+
 import './styles/styles.scss';
 
 // renderizzare il Component
