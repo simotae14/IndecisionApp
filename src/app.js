13148025c3a272f67ce29b47be146891488cc5cd
Simone Taeggi <simone.taeggi@leovegas.com>
2018-02-21 19:07:35 +0100

    first commit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp'
-
 import 'normalize.css/normalize.css';
-
 import './styles/styles.scss';
 
 // renderizzare il Component
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
